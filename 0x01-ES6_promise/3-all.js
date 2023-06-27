fb35f7103a661876dd0ffe03174ee98b8a855c33
2022phyro <phyrokelstein2@gmail.com>
2023-06-27 20:21:30 +0100

    attempted no 1 to 5 and tried to fix all 'lint' errors

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,10 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then((results) => {
+      const [res, res2] = results;
+      console.log(`${res.body} ${res2.firstName} ${res2.lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
+}
