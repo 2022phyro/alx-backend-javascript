9fb3f0606a37c92bbd258c65b5fec6c63dcc4dc2
2022phyro <phyrokelstein2@gmail.com>
2023-06-27 21:43:34 +0100

    Attempted the advanced tasks and removed the main.js files

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -6,6 +6,6 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
     .all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((results) => results.map((result) => ({
       status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
+      value: result.status === 'fulfilled' ? result.value : result.reason.toString(),
     })));
 }
