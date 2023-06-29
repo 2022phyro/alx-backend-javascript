baeee50f8602bc21e78c104bb6d1c33023536b0b
2022phyro <phyrokelstein2@gmail.com>
2023-06-29 17:53:32 +0100

    Attempted no 5

--- 0x02-ES6_classes/3-main.js
+++
@@ -1,7 +0,0 @@
-import Currency from './3-currency.js';
-
-const dollar = new Currency('$', 'Dollars');
-console.log(dollar);
-console.log(dollar.displayFullCurrency());
-console.log(typeof dollar instanceof Currency);
-console.log(typeof dollar);
---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,18 @@
+export default class Building {
+    constructor(sqft) {
+        this.sqft = sqft;
+    }
+
+    get sqft() {
+        return this._sqft;
+    }
+    set sqft(val) {
+        if (typeof val !== 'number') {
+            throw new TypeError('Sqft must be a number');
+        }
+        this._sqft = val;
+    }
+    evacuationWarningMessage() {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+}
