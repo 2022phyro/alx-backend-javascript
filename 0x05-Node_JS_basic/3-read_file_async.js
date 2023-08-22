74bc6a9b43ee703b6c7eb925b9276aab60c45e30
2022phyro <phyrokelstein2@gmail.com>
2023-08-22 16:53:14 +0100

    Finally got eslint working

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,31 +1,31 @@
-fs = require('fs/promises')
+const fs = require('fs/promises');
+
 function countStudents(fil) {
-   return new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     fs.readFile(fil, 'utf-8')
-    .then((data) => {
-        fields = data.split('\n').filter(Boolean)
-        fields.shift()
+      .then((data) => {
+        const fields = data.split('\n').filter(Boolean);
+        fields.shift();
         // console.log(fields)
-        let students = {}
-        fields.forEach(line => {
-            let curr = line.split(',').filter(Boolean)
-            let fi = curr[3]
-            if (Object.keys(students).includes(fi)) {
-                students[fi].push(curr[0])
-            }
-            else {
-                students[fi] = [curr[0]]
-            }
+        const students = {};
+        fields.forEach((line) => {
+          const curr = line.split(',').filter(Boolean);
+          const fi = curr[3];
+          if (Object.keys(students).includes(fi)) {
+            students[fi].push(curr[0]);
+          } else {
+            students[fi] = [curr[0]];
+          }
         });
-        console.log(`Number of students: ${fields.length}`)
-        Object.keys(students).forEach(key => {
-            console.log(`Number of students in ${key}: ${students[key].length}. List: ${students[key].join(', ')}`)
-        })
-        resolve()
-    })
-    .catch((error) => {
-        reject(new Error('Cannot load the database'));
+        console.log(`Number of students: ${fields.length}`);
+        Object.keys(students).forEach((key) => {
+          console.log(`Number of students in ${key}: ${students[key].length}. List: ${students[key].join(', ')}`);
+        });
+        resolve();
       })
-    })
+      .catch(() => {
+        reject(new Error('Cannot load the database'));
+      });
+  });
 }
-module.exports = countStudents
+module.exports = countStudents;
