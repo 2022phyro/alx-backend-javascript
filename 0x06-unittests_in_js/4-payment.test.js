89e0ddd7e08afd03e6d8ce02833f660d516fa78f
2022phyro <phyrokelstein2@gmail.com>
2023-08-24 15:15:46 +0100

    no 4

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,21 @@
+const sinon = require('sinon');
+
+const chai = require('chai');
+
+const sendPaymentRequestToApi = require('./3-payment');
+
+const { expect } = chai;
+
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', () => {
+  it('checks that tsendPaymentRequest to api uses Utils.calculateNumber', () => {
+    const CNSpy = sinon.stub(Utils, 'calculateNumber').returns(10);
+    const CSpy = sinon.spy(console, 'log')
+    sendPaymentRequestToApi(100, 20);
+    expect(CNSpy.calledWith('SUM', 100, 20)).to.be.true;
+    expect(CSpy.calledWith('The total is: 10')).to.be.true;
+    CNSpy.restore();
+    CSpy.restore();
+  });
+});
