3fe948b25cf110a3c369dec61235c0d720e2dcb5
2022phyro <phyrokelstein2@gmail.com>
2023-08-24 11:00:05 +0100

    Debugging no 3

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,5 +1,3 @@
-import Utils from './utils';
-
 const sinon = require('sinon');
 
 const chai = require('chai');
@@ -8,22 +6,17 @@ const sendPaymentRequestToApi = require('./3-payment');
 
 const { expect } = chai;
 
+const Utils = require('./utils');
+
 describe('sendPaymentRequestToApi', () => {
   it('checks that tsendPaymentRequest to api uses Utils.calculateNumber', () => {
-    const calculateNumberSpy = sinon.spy();
+    const CNSpy = sinon.spy(Utils);
 
-    // Replace the actual Utils.calculateNumber with the spy
-    const calc = Utils.calculateNumber;
-    Utils.calculateNumber = calculateNumberSpy;
-
-    // Call the function to be tested
     sendPaymentRequestToApi(100, 20);
 
-    // Assertions
-    expect(calculateNumberSpy.calledOnce).to.be.true;
-    expect(calculateNumberSpy.calledWith('SUM', 100, 20)).to.be.true;
+    expect(CNSpy.calculateNumber.calledOnce).to.be.true;
+    expect(CNSpy.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
 
-    // Restore the original Utils.calculateNumber function
-    Utils.calculateNumber = calc;
+    CNSpy.calculateNumber.restore();
   });
 });
